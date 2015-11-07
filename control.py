7039658bb85fd4baba0a0d2aaf642d98abbc46a6
Nick Pesce <npesce@terpmail.umd.edu>
2015-11-07 18:42:36 +0000

    control now sends a response to the sender if an error occurs(wrong arguments)

--- control.py
+++ control.py
@@ -1,5 +1,13 @@
 import neopixels, socket, threading, time
 
+
+def start(args, t_stop,  addr):
+    error = neopixels.start(args, t_stop)
+    if not error is None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(error, addr)
+
+
 t_stop = threading.Event()
 t = None
 # the public network interface
@@ -18,7 +26,8 @@ while True:
     #Reset the flag for the next thread
     t_stop.clear()
     #Thread is needed for the script can run and this can listen for new commands at the same time
-    t = threading.Thread(target=neopixels.start, args=(data.split(), t_stop))
+    t = threading.Thread(target=start, args=(data.split(), t_stop, addr))
     t.daemon = True
     t.start()
+
 atexit.register(s.close)
--- start.py
+++ start.py
@@ -2,10 +2,9 @@ import socket
 
 UDP_IP = socket.gethostbyname("nickspi.student.umd.edu")
 UDP_PORT = 42297
-MESSAGE = "Hello, World!"
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT))
+response = sock.recvfrom(1024)
+print response
