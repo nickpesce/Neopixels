1759159cc5a6e28bc3ed7e52203e8106ebadf1fb
Nick Pesce <npesce@terpmail.umd.edu>
2015-11-07 18:47:35 +0000

    Fixed argument sending

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
-import socket
+import socket, sys
 
 UDP_IP = socket.gethostbyname("nickspi.student.umd.edu")
 UDP_PORT = 42297
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(" ".join(sys.argv[1:]), (UDP_IP, UDP_PORT))
 sock.bind((UDP_IP, UDP_PORT))
 response = sock.recvfrom(1024)
 print response
