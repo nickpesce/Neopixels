d081fcfbab2b7acef5c3af71eaf4ffd767fb8df4
Nick Pesce <npesce@terpmail.umd.edu>
2015-11-07 02:57:56 +0000

    Added a flag to stop the current effect and allow the next to start. Also added better help dialog

--- control.py
+++ control.py
@@ -1,19 +1,24 @@
 import neopixels, socket, threading, time
 
-global t
-
+t_stop = threading.Event()
+t = None
 # the public network interface
 HOST = "nickspi.student.umd.edu"
 
 # create a raw socket and bind it to the public interface
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 s.bind((HOST, 42297))
-while True:
-    global t
+while True:    
     data, addr = s.recvfrom(1024) # buffer size is 1024 bytes
-    neopixels.running = False
-    t.join()
-    t = threading.Thread(target=neopixels.start, args=(data.split(),))
+    #Setting the event makes the script halt
+    t_stop.set()
+    if not t is None:
+       #Wait for the script to finish before continuing
+        t.join()
+    #Reset the flag for the next thread
+    t_stop.clear()
+    #Thread is needed for the script can run and this can listen for new commands at the same time
+    t = threading.Thread(target=neopixels.start, args=(data.split(), t_stop))
     t.daemon = True
     t.start()
 atexit.register(s.close)
--- neopixels.py
+++ neopixels.py
@@ -1,18 +1,24 @@
-import np, time, random, math, sys, getopt
+import np, time, random, math, sys, getopt, threading
 
-def start(args):
+__author__="Nick Pesce"
+__email__="npesce@terpmail.umd.edu"
+
+global stop_event
+
+def start(args, stop=threading.Event()):
     """Determines effect  parameters to run with.
 
     Param args: The arguments passed in without the script name.
     args:
         - name of effect (Must be first)
         - -r [0, 255] -g [0, 255] -b [0, 255]. All or none.
-        - -s (0, infinity) speed. multiple of normal."""
-
+        - -s (0, infinity) speed. multiple of normal.
+    Param stop: thread stop event."""
+    global stop_event
+    stop_event = stop
     if len(args)<1:
-        print 'sudo python neopixels.py <effect> -s <speed>'
+        return help()
         sys.exit(2)
-    effect = effects[args[0]]
     speed = None
     r = None
     g = None
@@ -21,7 +27,7 @@ def start(args):
         #Separates the passed in arguments to option, argument tuples.
         opts, args = getopt.getopt(args[1:], "s:r:g:b:")
     except getopt.GetoptError:
-        print 'sudo python neopixels.py <effect> -s <speed>'
+        return help()
         sys.exit(2)
     for opt, arg in opts:
         #assign variables to passed in values
@@ -44,38 +50,46 @@ def start(args):
 
     #Call with the correct parameters based on what was passed in.
     # If parameters dod not match, an error will be thrown.
-    if hasSpeed and hasColor:
-        effect(speed = speed, r = r, g = g, b = b)
-    elif hasSpeed:
-        effect(speed = speed)
-    elif hasColor:
-        effect(r = r, g = g, b = b)
-    else:
-        effect()
+    try:
+        effect = effects[args[0]]
+        if hasSpeed and hasColor:
+            effect(speed = speed, r = r, g = g, b = b)
+        elif hasSpeed:
+            effect(speed = speed)
+        elif hasColor:
+            effect(r = r, g = g, b = b)
+        else:
+            effect()
+    except:
+        return help()
+
+def help():
+    return """sudo python neopixels.py <effect> [-s speed] [-r red -g green -b blue]
+Effects:\n    - """ + ("\n    - ".join(effects.keys()))
 
     
 def slide(speed = 1):
     """The full color spectrum is shown and it "slides"/translates across the string
 
     Param speed: How fast it slides. Scales the default speed."""
-
+    global stop_event
     off = 0
-    while(True):
+    while(not stop_event.is_set()):
         for n in range(0, 60):
             np.set_pixel_hsv(n, ((n+off)/60.0)%1, 1, 1)
         off+=.1
         np.show()
-        time.sleep(.05/speed)
+        stop_event.wait(.05/speed)
         
 def bounce(speed=1):
     """Two pixels start on either end and move along the string changing color.
     When the end is hit, they change  direction
 
     Param speed: Scales the default speed."""
-
+    global stop_event
     x = 0
     dx = .1
-    while(True):
+    while(not stop_event.is_set()):
         np.off()
         np.set_pixel_hsv(int(x), (x/60.0)%1, 1, 1)
         np.set_pixel_hsv(int(60-x), ((60-x)/60.0)%1, 1, 1)
@@ -83,63 +97,65 @@ def bounce(speed=1):
         if(x+dx >=60 or x+dx <0):
             dx = -dx
         np.show()
-        time.sleep(.001/speed)
+        stop_event.wait(.01/speed)
 
 def christmas(speed=1):
     """Lights up green and red. Pattern "slides" along the string.
 
     Param speed: Scales the default speed"""
 
+    global stop_event
     for n in range(0, 60):
         x = math.fabs((30 - n)/30.0)
         np.set_pixel(n, int(255-(x*255)), int(x*255), 0)
             
-    while(True):
+    while(not stop_event.is_set()):
         first = np.get_pixel(0)
         for n in range(0, 59):
             np.set_pixel(n, *np.get_pixel(n+1))
             np.set_pixel(59, *first)
         np.show()
-        time.sleep(.2/speed)
+        stop_event.wait(.2/speed)
         
 def cycle(speed=1):
     """Cycles through the color spectrum. Entire string is the same color.
     
     Param speed: Scales the default speed."""
 
-    global running
-    running = True
-    while(running):
-        for h in range(0, 10000):
-            np.set_all_pixels_hsv(h/10000.0, 1, 1)
+    global stop_event
+    while not stop_event.is_set():
+        for h in range(0, 1000):
+            if stop_event.is_set():
+                break
+            np.set_all_pixels_hsv(h/1000.0, 1, 1)
             np.show()
-            time.sleep(.05/speed)
+            stop_event.wait(.05/speed)
 
 def rave(speed=1):
     """Each individual light displays a different random color, changing rapidly.
 
     Param speed: Scales the default speed."""
 
-    global running
-    running = True
-    while(running):
+    global stop_event
+    while(not stop_event.is_set()):
         for n in range(0, 60):
             np.set_pixel(n, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         np.show();
-        time.sleep(.1/speed)
+        stop_event.wait(.1/speed)
 
 def strobe(speed=1, r=255, g=255, b=255):
     """Entire string flashes rapidly.
     
     Param speed: Scales the default speed.
     Param r, g, b: Default white. RGB values for light color. [0, 255]"""
-
-    while(True):
+    
+    global stop_event
+    while(not stop_event.is_set()):
         np.set_all_pixels(r, g, b)
         np.show()
-        time.sleep(.1/speed)
+        stop_event.wait(.1/speed)
         np.off()
-        time.sleep(.1/speed)
+        stop_event.wait(.1/speed)
         
 def throb(speed=1, r=255, b=255, g=255):
     """Entire string cycles through brightness levels. Starts off, gradually gets brighters, the darker, and repeats.
@@ -147,24 +163,26 @@ def throb(speed=1, r=255, b=255, g=255):
     Param speed: Scales the default speed.
     Param r, g, b: Default white. RGB values for light color. [0, 255]"""
 
+    global stop_event
     np.set_all_pixels(r, g, b)
     brightness = 0
     db = .01
-    while(True):
+    while(not stop_event.is_set()):
         np.brightness(brightness)
         np.show()
         if brightness + db > 1 or brightness + db < 0:
             db = -db
         brightness += db
-        time.sleep(.01/speed)
+        stop_event.wait(.01/speed)
 
 def on(r = 255, g = 255, b = 255):
     """Turns the entire string on.
     Param r, g, b: Default white. RGB values for light color. [0, 255]"""
 
+    global stop_event
     np.set_all_pixels(r, g, b)
     np.show()
-    while True:
+    while not stop_event.is_set():
         time.sleep(1)
 
 def disco(speed=1):
@@ -172,13 +190,14 @@ def disco(speed=1):
     
     Param speed: Scales the default speed."""
 
+    global stop_event
     off = 0
-    while(True):
+    while not stop_event.is_set():
         for n in range(0, 60):
             np.set_pixel_hsv(n, ((n*off)/60.0)%1, 1, 1)
         off+=.1
         np.show()
-        time.sleep(.05/speed)
+        stop_event.wait(.05/speed)
 
 def chase(speed = 1):
     """Each light sequentially lights up a color untill the string is filled with that color,
@@ -186,12 +205,15 @@ def chase(speed = 1):
 
     Param speed: Scales the default speed."""
 
+    global stop_event
     hue = 0;
-    while True:
+    while not stop_event.is_set():
         for n in range(0, 60):
+            if stop_event.is_set():
+                break
             np.set_pixel_hsv(n, hue, 1, 1)
             np.show()
-            time.sleep(.05/speed)
+            stop_event.wait(.05/speed)
         hue += .2
         hue %= 1
     
@@ -214,4 +236,6 @@ effects = {'cycle' : cycle,
     }
 
 if __name__ == "__main__":
-    start(sys.argv[1:])
+    error = start(sys.argv[1:])
+    if not error is None:
+        print error
