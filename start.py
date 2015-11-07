c5fcbfc379ce972b8cad6a817a6c4fccceca4a35
Nick Pesce <npesce@terpmail.umd.edu>
2015-11-07 21:03:20 +0000

    Fixed return from receive. Added address.

--- start.py
+++ start.py
@@ -6,5 +6,5 @@ UDP_PORT = 42297
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.sendto(" ".join(sys.argv[1:]), (UDP_IP, UDP_PORT))
 sock.bind((UDP_IP, UDP_PORT))
-response = sock.recvfrom(1024)
+response, addr = sock.recvfrom(1024)
 print response
